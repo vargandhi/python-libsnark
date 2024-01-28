84914bd13f884a14abe0352034e855b5ed4f067d
Varun Gandhi <vgandhi@g.harvard.edu>
2024-01-28 12:24:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ os.system("swig -python -c++ -o alt_bn128_wrap.cpp libsnark.i")
 setup(name = "python-libsnark",
       version = "0.3.3",
       description='Python bindings for a restricted subset of libsnark',
-      author='Meilof Veeningen',
+      author='Varun Gandhi',
       author_email='meilof@gmail.com',
       license='MIT',
-      url='https://github.com/meilof/python-libsnark',
+      url='https://github.com/vargandhi/python-libsnark',
       packages = ["libsnark"],
       package_dir = {"libsnark": "."},
       ext_modules = [Extension("libsnark._alt_bn128", ["alt_bn128_wrap.cpp"], extra_compile_args=["-std=c++14", "-Wno-sign-compare", "-Wno-delete-non-virtual-dtor", "-Wno-unused-variable", "-DCURVE_ALT_BN128", "-DBN_SUPPORT_SNARK=1", "-DMONTGOMERY_OUTPUT", "-DNO_PROCPS", "-I/usr/local/Cellar/openssl@1.1/1.1.1i/include"], libraries=["snark", "ff", "gmpxx", "boost_iostreams"], swig_opts=["-c++"])])
